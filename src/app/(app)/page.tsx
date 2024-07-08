59898fd524aa32dca08b37c5f5f43243c5d8a885
sirily11 <32106111+sirily11@users.noreply.github.com>
2024-07-09 01:13:59 +0800

    fix: add dialog

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -11,27 +11,39 @@ export default async function Page() {
   const translation = await getDictionary("zh");
 
   return (
-    <main className="flex flex-col">
-      <Component title={translation["welcome-title"]} />
-      <Component2
-        titleText={translation["tokyo-title"]}
-        tokyoFireworksTitle={translation["tokyo-firework-title"]}
-      />
-      <Component3 title={translation["kyoto-title"]} />
-      <Component4 title={translation["japan-city-title"]} />
-      <Component5
-        title={translation["chapter-5-title"]}
-        title2={translation["chapter-5-title2"]}
-        title3={translation["chapter-5-title3"]}
-      />
-      <Component6
-        title={translation["chapter-6-title"]}
-        title2={translation["chapter-6-title2"]}
-      />
-      <Component7
-        title={translation["chapter-7-title"]}
-        claimButton={translation["claim-button"]}
-      />
+    <main className="flex">
+      <div className={"mx-auto max-w-md w-full max-h-[1000px] my-auto"}>
+        <Component title={translation["welcome-title"]} />
+        <Component2
+          titleText={translation["tokyo-title"]}
+          tokyoFireworksTitle={translation["tokyo-firework-title"]}
+        />
+        <Component3 title={translation["kyoto-title"]} />
+        <Component4 title={translation["japan-city-title"]} />
+        <Component5
+          title={translation["chapter-5-title"]}
+          title2={translation["chapter-5-title2"]}
+          title3={translation["chapter-5-title3"]}
+        />
+        <Component6
+          title={translation["chapter-6-title"]}
+          title2={translation["chapter-6-title2"]}
+        />
+        <Component7
+          title={translation["chapter-7-title"]}
+          claimButton={translation["claim-button"]}
+          dialog={{
+            title: translation["dialog-title"],
+            description: translation["dialog-content"],
+            confirm: translation["dialog-button"],
+            cancel: translation["dialog-button2"],
+            description2: translation["dialog-content2"],
+            description3: translation["dialog-content3"],
+            description4: translation["dialog-content4"],
+            previous: translation["dialog-button3"],
+          }}
+        />
+      </div>
     </main>
   );
 }
--- src/components/chapter7/Component7.tsx
+++ src/components/chapter7/Component7.tsx
@@ -3,10 +3,28 @@ import * as React from "react";
 import { BannerLayer, ParallaxBanner } from "react-scroll-parallax";
 import Image from "next/image";
 import { Ma_Shan_Zheng } from "next/font/google";
+import {
+  Description,
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import { useCallback } from "react";
 
 type Props = {
   title: string;
   claimButton: string;
+  dialog: {
+    title: string;
+    description: string;
+    description2: string;
+    description3: string;
+    description4: string;
+    confirm: string;
+    cancel: string;
+    previous: string;
+  };
 };
 
 const font = Ma_Shan_Zheng({
@@ -15,6 +33,37 @@ const font = Ma_Shan_Zheng({
 });
 
 export function Component7(props: Props) {
+  const [open, setOpen] = React.useState(false);
+  const [chapter, setChapter] = React.useState(0);
+
+  const handleOpen = useCallback(() => {
+    setOpen(true);
+    setChapter(0);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setOpen(false);
+    setChapter(0);
+  }, []);
+
+  const handleClaim = useCallback(() => {
+    if (chapter === 0) {
+      setChapter(1);
+    } else if (chapter === 1) {
+      setChapter(2);
+    } else if (chapter === 2) {
+      // claim
+    }
+  }, [chapter]);
+
+  const handlePrevious = useCallback(() => {
+    if (chapter === 1) {
+      setChapter(0);
+    } else if (chapter === 2) {
+      setChapter(1);
+    }
+  }, [chapter]);
+
   const claimButton: BannerLayer = {
     shouldAlwaysCompleteAnimation: true,
     expanded: true,
@@ -27,7 +76,7 @@ export function Component7(props: Props) {
           className={
             "w-40 active:scale-125 text-yellow-950 active:text-yellow-800 relative"
           }
-          onClick={() => {}}
+          onClick={handleOpen}
         >
           <Image
             src={"/assets/button.svg"}
@@ -72,20 +121,6 @@ export function Component7(props: Props) {
     ),
   };
 
-  const foreground: BannerLayer = {
-    shouldAlwaysCompleteAnimation: true,
-    speed: 10,
-    children: (
-      <Image
-        src={"/assets/new-york1.png"}
-        alt={"new-york"}
-        width={1000}
-        height={1000}
-        className={"absolute bottom-0 h-[600px] object-cover"}
-      />
-    ),
-  };
-
   const kuromi: BannerLayer = {
     translateY: [0, 20],
     children: (
@@ -111,9 +146,100 @@ export function Component7(props: Props) {
   };
 
   return (
-    <ParallaxBanner
-      layers={[background, headline, kuromi, claimButton]}
-      className="h-[150vh] bg-gray-900"
-    />
+    <div>
+      <ParallaxBanner
+        layers={[background, headline, kuromi, claimButton]}
+        className="h-[150vh] bg-gray-900"
+      />
+      <Dialog
+        onClose={() => {}}
+        open={open}
+        transition
+        className="relative z-50 transition duration-300 ease-out data-[closed]:opacity-0"
+      >
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          <DialogPanel className="w-screen space-y-4 border bg-white p-12 rounded-3xl outline outline-gray-200">
+            <DialogTitle className="font-bold">
+              {props.dialog.title}
+            </DialogTitle>
+            <DialogBackdrop className="fixed inset-0 bg-black/70 -z-10" />
+            <Description>
+              {chapter === 0 && props.dialog.description}
+              {chapter === 1 && props.dialog.description2}
+              {chapter === 2 && (
+                <span>
+                  {props.dialog.description3}
+                  <span className={"text-orange-400 font-bold"}>
+                    {props.dialog.description4}
+                  </span>
+                </span>
+              )}
+
+              {chapter === 2 && (
+                <div className={"flex justify-center relative"}>
+                  <Image
+                    src={"/assets/birthday-cake.svg"}
+                    alt={"coin"}
+                    width={100}
+                    height={100}
+                    className={"w-72 aspect-auto z-10"}
+                  />
+                  <Image
+                    src={"/assets/2.svg"}
+                    alt={"coin"}
+                    width={100}
+                    height={100}
+                    className={
+                      "w-14 aspect-auto absolute top-5 left-[68px] rotating-object-right-left"
+                    }
+                  />
+                  <Image
+                    src={"/assets/5.svg"}
+                    alt={"coin"}
+                    width={100}
+                    height={100}
+                    className={
+                      "w-14 aspect-auto absolute top-5 right-[68px] rotating-object-left-right"
+                    }
+                  />
+                  <Image
+                    src={"/assets/Kuromi_2.svg"}
+                    alt={"coin"}
+                    width={100}
+                    height={100}
+                    className={
+                      "w-14 aspect-auto absolute top-5 z-20 rotating-object-left-right"
+                    }
+                  />
+                </div>
+              )}
+            </Description>
+            <hr />
+            <div className="flex gap-4 justify-between">
+              {chapter === 0 && (
+                <button
+                  className={"active:text-gray-500"}
+                  onClick={handleClose}
+                >
+                  {props.dialog.cancel}
+                </button>
+              )}
+
+              {chapter !== 0 && (
+                <button
+                  className={"active:text-gray-500"}
+                  onClick={handlePrevious}
+                >
+                  {props.dialog.previous}
+                </button>
+              )}
+              <button className={"active:text-gray-500"} onClick={handleClaim}>
+                {props.dialog.confirm}
+              </button>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </div>
   );
 }
