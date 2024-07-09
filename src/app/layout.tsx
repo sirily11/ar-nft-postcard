bb8fe0bfce2034f19b98cb1791a7aa3e98394fa0
sirily11 <32106111+sirily11@users.noreply.github.com>
2024-07-09 23:51:07 +0800

    fix: add open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Happy Birthday!",
+  description:
+    "Happy 25th, Lin Yanjun! May your year be filled with joy and success.",
+  openGraph: {
+    images: ["/HappyBirthdayCover.png"],
+    title: "Happy Birthday Lin Yanjun!",
+    description:
+      "Happy 25th, Lin Yanjun! May your year be filled with joy and success.",
+  },
 };
 
 export default function RootLayout({
